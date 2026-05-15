aef04f3c63328ef3b361f9031c7cae808bbf3d37
Lachlann Lees <23367810@studentmail.ucol.ac.nz>
2026-05-16 00:02:31 +1200

    Commit 8: Added Pokemon search bar that goes to designated page

--- app/pokemons/page.tsx
+++ app/pokemons/page.tsx
@@ -1,9 +1,33 @@
 "use client";
 
+import { useState } from "react";
+import { useRouter } from "next/navigation"
+
 export default function PokemonPage(){
+    const [search, setSearch] = useState("");
+    const router = useRouter();
+
+    const handleSearch = () => {
+        if (!search) return;
+
+        router.push(`/pokemons/${search.toLowerCase()}`);
+    }
+
+
     return(
         <div>
             <h1>Pokemon List</h1>
+
+            <input 
+            type="text"
+            placeholder="Search Pokemon"
+            value={search}
+            onChange={(e) => setSearch(e.target.value)} 
+            />
+
+            <button onClick={handleSearch}>
+                Search
+            </button>
         </div>
     )
 }
