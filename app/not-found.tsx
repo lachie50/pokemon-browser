68a93c96fcd92f948b2e36fe93a000f994dd116b
Lachlann Lees <23367810@studentmail.ucol.ac.nz>
2026-05-16 20:59:07 +1200

    Commit 15: styled error when pokemon doesnt exist is searched

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,7 +1,7 @@
 export default function NotFound() {
     return (
-        <div>
-            <h1>404 - Pokemon Not Found</h1>
+        <div className="flex items-center justify-center min-h-[80vh]">
+            <h1 className="text-5xl font-bold text-black-600">404 - Pokemon Not Found</h1>
         </div>
     );
 }
