cd188399192dce5c82af726e87475a7b3ddba1c7
Lachlann Lees <23367810@studentmail.ucol.ac.nz>
2026-05-16 20:29:43 +1200

    Commit 13: basic styling on pokemon search page

--- app/pokemons/page.tsx
+++ app/pokemons/page.tsx
@@ -15,19 +15,27 @@ export default function PokemonPage(){
 
 
     return(
-        <div>
-            <h1>Pokemon List</h1>
+        <div className="flex flex-col items-center justify-center min-h-[80vh] px-4 text-center">
+            <h1 className="text-3xl font-bold text-black-600 mb-6">Pokemon Search</h1>
 
+        <div className="flex gap-3">
             <input 
             type="text"
             placeholder="Search Pokemon"
             value={search}
             onChange={(e) => setSearch(e.target.value)} 
+            className="border-2 border-black px-4 py-2 rounded-full"
             />
 
-            <button onClick={handleSearch}>
+            <button 
+                onClick={handleSearch}
+                className="bg-red-600 text-white px-5 py-2 rounded-full border-2 border-black hover:bg-red-700 transition">
                 Search
             </button>
         </div>
+
+
+        </div>
+ 
     )
 }
