bec33532e9c5e4b539e30319838338504ee55532
Lachlann Lees <23367810@studentmail.ucol.ac.nz>
2026-05-16 20:54:55 +1200

    Commit 14: Simple style to dynamic route page, Pokemons stats page

--- app/pokemons/[name]/page.tsx
+++ app/pokemons/[name]/page.tsx
@@ -15,61 +15,79 @@ export default async function PokemonDetailsPage({ params }: { params: { name: s
     const pokemon = await response.json()
 
     return (
-        <div>
-            <h1>{pokemon.name}</h1>
+        <div className="min-h-[80vh] px-4 py-10 flex flex-col items-center">
+
+          <h1 className="text-4xl font-bold text-black-600 capitalize mb-8">{pokemon.name}</h1>
 
             
-        <div style={{ display: "flex", gap: "20px" }}>
+        <div className="flex gap-6 mb-10">
+          <div className="text-center">
+            <Image
+            src={pokemon.sprites.front_default}
+            alt={`${pokemon.name} front`}
+            width={150}
+            height={150}
+            />
+            <p className="text-sm text-gray-500">Front</p>
+          </div>
+
+          <div className="text-center">
+            <Image
+            src={pokemon.sprites.back_default}
+            alt={`${pokemon.name} back`}
+            width={150}
+            height={150}
+            />
+            <p className="text-sm text-gray-500">Back</p>
+          </div>
+
+
+          <div className="text-center">
+            <Image
+            src={pokemon.sprites.front_shiny}
+            alt={`${pokemon.name} shiny`}
+            width={150}
+            height={150}
+            />
+            <p className="text-sm text-gray-500">Shiny</p>
+          </div>
         
-        <Image
-          src={pokemon.sprites.front_default}
-          alt={`${pokemon.name} front`}
-          width={150}
-          height={150}
-        />
-
-        <Image
-          src={pokemon.sprites.back_default}
-          alt={`${pokemon.name} back`}
-          width={150}
-          height={150}
-        />
-
-        <Image
-          src={pokemon.sprites.front_shiny}
-          alt={`${pokemon.name} shiny`}
-          width={150}
-          height={150}
-        />
-
-        <Image
+          <div className="">
+            <Image
             src={pokemon.sprites.back_shiny}
             alt={`${pokemon.name} shiny back`}
             width={150}
             height={150}
             />
+            <p className="text-sm text-gray-500">Shiny Back</p>
+          </div>
       </div>
 
-      <div>
-        <h2>Types</h2>
+      <div className="mb-8 text-center">
+        <h2 className="text-xl font-semibold mb-2">Types</h2>
 
         {pokemon.types.map((type: any) => (
-          <p key={type.type.name}>
+          <p 
+            key={type.type.name}
+            className="capitalize"
+          >
             {type.type.name}
           </p>
         ))}
       </div>
 
-      <div>
-        <h2>Base Stats</h2>
+      <div className="text-center">
+        <h2 className="text-xl font-semibold mb-2">Base Stats</h2>
 
         {pokemon.stats.map((stat: any) => (
-          <p key={stat.stat.name}>
+          <p
+            key={stat.stat.name}
+            className="capitalize"
+          >
             {stat.stat.name}: {stat.base_stat}ad
           </p>
         ))}
-      </div>
-            
-        </div>
-    )
+      </div>   
+    </div>
+   )
 }
